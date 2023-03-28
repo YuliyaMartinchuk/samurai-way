9bf831173bbfecfad0555e15843a348a11960262
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-28 23:42:52 +0300

    added component Navbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 
 const App = () => {
     return (
         <div className="app-wrapper">
            <Header />
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Message</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
+            <Navbar />
 
-            </nav>
             <div className="content">
                 <div>
                 <img src="https://i.pinimg.com/564x/61/1f/56/611f56fa4ed62d08428296604b568624.jpg"/>
