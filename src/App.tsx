5a99d9d1a92c342eb31d1af60f65306753f0e345
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-28 23:46:05 +0300

    added component Profile.tsx.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,17 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 const App = () => {
     return (
         <div className="app-wrapper">
-           <Header />
+            <Header />
             <Navbar />
-
-            <div className="content">
-                <div>
-                <img src="https://i.pinimg.com/564x/61/1f/56/611f56fa4ed62d08428296604b568624.jpg"/>
-                </div>
-
-            <div>
-                ava + description
-            </div>
-            <div>
-                My posts
-                <div>
-                    New posts
-                </div>
-                <div>
-                    <div>
-                        Post 1
-                    </div>
-                    <div>
-                        Post 3
-                    </div>
-                </div>
-            </div>
-            </div>
-
-</div>
-)
-    ;
+            <Profile />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (<div className="content">
+        <div>
+            <img src="https://i.pinimg.com/564x/61/1f/56/611f56fa4ed62d08428296604b568624.jpg"/>
+        </div>
+
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 3
+                </div>
+            </div>
+        </div>
+    </div>)
+}
+export default Profile
