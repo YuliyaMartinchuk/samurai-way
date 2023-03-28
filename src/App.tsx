41bc35a47e2f299971f155a61a70be485d104a5b
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-03-28 23:32:47 +0300

    added component Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+import Header from "./components/Header";
 
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className="app-wrapper">
+           <Header />
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Message</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
 
-        </div>
-    );
+            </nav>
+            <div className="content">
+                <div>
+                <img src="https://i.pinimg.com/564x/61/1f/56/611f56fa4ed62d08428296604b568624.jpg"/>
+                </div>
+
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 3
+                    </div>
+                </div>
+            </div>
+            </div>
+
+</div>
+)
+    ;
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+    return (<header className="header">
+        <img src="https://i.pinimg.com/564x/9f/ed/b6/9fedb6684c3b442d9583724d08e1a14d.jpg"/>
+    </header>)
+}
+export default Header
