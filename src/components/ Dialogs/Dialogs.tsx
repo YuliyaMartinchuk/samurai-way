bc12e9954d4b915050363a936ecb2b870582488d
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-04-04 18:47:45 +0300

    lesson 21(added  style for dialogs, added messages)

--- src/components/ Dialogs/Dialogs.tsx
+++ src/components/ Dialogs/Dialogs.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import s from "Dialogs.module.css"
+import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
-    return ( <div >
-            Dialogs
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Yuliya
+                </div>
+                <div className={s.dialog}>
+                    Yuliya
+                </div>
+                <div className={s.dialog}>
+                    Yuliya
+                </div>
+                <div className={s.dialog}>
+                    Yuliya
+                </div>
+                <div className={s.dialog}>
+                    Yuliya
+                </div>
+                <div className={s.dialog}>
+                    Yuliya
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>llll</div>
+                <div className={s.message}>llll</div>
+                <div className={s.message}>llll</div>
+
+            </div>
+
         </div>
 
     )
