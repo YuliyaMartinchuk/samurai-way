bc76a8719e55abeca8c1ec02d55188e75ca84b30
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-04-05 11:29:08 +0300

    lesson 22(added  link for items dialogs)

--- src/components/ Dialogs/Dialogs.tsx
+++ src/components/ Dialogs/Dialogs.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + " " + s.active}>
-                    Yuliya
+                    <NavLink to="/dialogs/1">Ann</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Yuliya
+                    <NavLink to="/dialogs/1">Valera</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Yuliya
+                    <NavLink to="/dialogs/1">Maks</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Yuliya
+                    <NavLink to="/dialogs/1">Alex</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Yuliya
+                    <NavLink to="/dialogs/1">Nick</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Yuliya
+                    <NavLink to="/dialogs/1">Tim</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
-                <div className={s.message}>llll</div>
-                <div className={s.message}>llll</div>
-                <div className={s.message}>llll</div>
+                <div className={s.message}>Hello!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Thank you</div>
 
             </div>
 
