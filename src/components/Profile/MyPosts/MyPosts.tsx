c1a06c78cd6ee29d2c408d38a75f82cc9c47b016
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-04-21 00:52:32 +0300

    lesson 31(onClick, ref)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {LegacyRef} from "react";
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 import {PostPropsType, ProfilePagePropsType, RootStateType} from "../../../redux/state";
@@ -6,6 +6,7 @@ import {PostPropsType, ProfilePagePropsType, RootStateType} from "../../../redux
 
 type PropsType = {
     profilePage: ProfilePagePropsType
+
 }
 
 const MyPosts = (props:PropsType) => {
@@ -13,15 +14,22 @@ const MyPosts = (props:PropsType) => {
     let postsElement =
         props.profilePage.posts.map(p=><Post message={p.message} likesCount={p.likesCount} />)
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
+
+    const addPost = () => {
+        let text = newPostElement.current?.value
+            alert(text)
+    }
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post </button>
+                    <button onClick={addPost}>Add post </button>
                 </div>
 
             </div>
