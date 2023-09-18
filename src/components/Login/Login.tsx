ff36d5d01902272991a614cc807d6426f61ab31c
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-09-18 23:46:29 +0300

    lesson 75 - added component LoginForm

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+export const LoginForm = () => {
+    return (
+        <form>
+            <div>
+                <input placeholder={"Login"}/>
+            </div>
+            <div>
+                <input placeholder={"Password"}/>
+            </div>
+            <div>
+                <input type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+
+};
 
 export const Login = () => {
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginForm/>
+    </div>
 };
 
