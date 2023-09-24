71ab4052764a980c497920344a48b47c8ae966b6
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-09-24 23:26:31 +0300

    lesson 85 - added useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,5 +1,6 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import s from "./Profileinfo.module.css"
+
 
 
 type PropsType = {
@@ -11,6 +12,11 @@ export const ProfileStatusWithHooks: React.FC<PropsType> = (props) => {
     const {status, updateStatus} = props
     const [editMode, setEditMode] = useState<boolean>(false)
     const [localStatus, setLocalStatus] = useState<string>(status)
+
+
+    useEffect(() => {
+        setLocalStatus(status)
+    }, [status])
 
     const onStatusChanged = (e: ChangeEvent<HTMLInputElement>) => {
         setLocalStatus(e.currentTarget.value)
