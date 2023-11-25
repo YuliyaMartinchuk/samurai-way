3fd2f4d0db72ebf5ba3dafb6f3b3ec128898e6c9
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-11-24 22:37:25 -0800

    feat: added test "success unfollow thunk". Used beforeEach

--- src/redux/reducers/usersReducer.thunks.test.ts
+++ src/redux/reducers/usersReducer.thunks.test.ts
@@ -1,11 +1,24 @@
 
-import {followTC} from "../thunks/usersThunks";
+import {followTC, unFollowTC} from "../thunks/usersThunks";
 import {usersAPI} from "../../api/usersApi";
-import {follow, toggleFollowingProgress} from "../actions/usersAction";
+import {
+    follow,
+    toggleFollowingProgress,
+    unFollow
+} from "../actions/usersAction";
 
 jest.mock("../../api/usersApi")
 
 const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>
+
+
+const dispatchMock = jest.fn()
+const getStateMock = jest.fn()
+
+beforeEach(() => {
+    dispatchMock.mockClear()
+    getStateMock.mockClear()
+})
 
 const result = {
     resultCode: 0,
@@ -15,11 +28,10 @@ const result = {
 }
 
 usersAPIMock.follow.mockReturnValue(Promise.resolve(result))
+usersAPIMock.unFollow.mockReturnValue(Promise.resolve(result))
 test("success follow thunk", async () => {
 
     const thunk = followTC(1)
-    const dispatchMock = jest.fn()
-    const getStateMock = jest.fn()
 
     await thunk(dispatchMock, getStateMock,{})
 
@@ -28,4 +40,17 @@ test("success follow thunk", async () => {
     expect(dispatchMock).toHaveBeenCalledWith(2, follow( 1))
     expect(dispatchMock).toHaveBeenCalledWith(3, toggleFollowingProgress(false, 1))
 })
+
+test("success unfollow thunk", async () => {
+
+    const thunk = unFollowTC(1)
+
+    await thunk(dispatchMock, getStateMock,{})
+
+    expect(dispatchMock).toBeCalledTimes(1)
+    expect(dispatchMock).toHaveBeenCalledWith(1, toggleFollowingProgress(true, 1))
+    expect(dispatchMock).toHaveBeenCalledWith(2, unFollow( 1))
+    expect(dispatchMock).toHaveBeenCalledWith(3, toggleFollowingProgress(false, 1))
+})
+
 
