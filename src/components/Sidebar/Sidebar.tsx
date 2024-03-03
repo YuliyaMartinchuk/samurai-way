2a0a816866a8bd742120e71d0b64c3be900e3337
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2024-03-03 20:30:35 +0300

    style: add style for Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,20 @@
 import Navbar from "./Navbar/Navbar";
 import {Contacts} from "./Contacts";
 import {SidebarPropsType} from "./SidebarContainer";
-
+import s from './Sidebar.module.css'
 export const Sidebar = (props:SidebarPropsType) => {
     const {users} = props
     return (
-        <div>
-        <div>
-            <Navbar/>
-        </div>
-        <div>
-            <Contacts contacts={users}/>
-        </div>
+        <div className={s.root}>
+            <div className={s.container}>
+                <div >
+                    <Navbar/>
+                </div>
+                <div>
+                    <Contacts contacts={users}/>
+                </div>
+            </div>
+
         </div>
     )
 }
